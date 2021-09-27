b21944197560848fb7d3100b8b7bd21b5ce78a8f
Mohamed Abo-ghazalla <45886535+Abo-ghazalla@users.noreply.github.com>
2021-09-27 17:47:10 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -69,9 +69,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ffff82a1641765d4d0c838766219919d",
-"/": "ffff82a1641765d4d0c838766219919d",
-"main.dart.js": "13d8944393b3891a6aa1522627589e5c",
+"index.html": "35308c817b3f682e22b4acb698fcec25",
+"/": "35308c817b3f682e22b4acb698fcec25",
+"main.dart.js": "a02418e6e31810ca15356d6567e8cfd3",
 "manifest.json": "c61be6c71392f19918d685ce24471255",
 "version.json": "e9d60ee4fcbe2bc2c45e987d9bee87c1"
 };
